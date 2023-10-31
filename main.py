6709b292aa08a5f5cfa14f2ce87f1f1dfa836994
Mauricio Matchal <mauriciomatchal@gmail.com>
2023-10-30 22:12:07 -0300

    modified:   main.py

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ class Prova(Image):
         self.file = "prova.png"
         self.x = x
         self.y = y
-        self.vel = 20
+        self.vel = 10
     def update(self):
         if self.vel:
             self.y += self.vel
+    def change_dif(self, dif):
+        match dif:
+            case "easy":
+                self.vel = 10
+            case "medium":
+                self.vel = 15
+            case "hard":
+                self.vel = 20
 class Player(Image):
     def __init__(self, x: int, y: int) -> None:
         self.file = "humandown.png"
@@ -16,16 +24,19 @@ class Player(Image):
         self.y = y
         self.direction = ""
     def update(self):
-        if self.direction == "left":
-            self.file = "humanleft.png"
-        if self.direction == "right":
-            self.file = "humanright.png"
+        match self.direction:
+            case "left":
+                self.file = "humanleft.png"
+            case "right":
+                self.file = "humanright.png"
+                
         if keyboard.is_key_down('Left'):
             self.direction = "left"
-            self.x -= 20
+            self.x -= 15
         if keyboard.is_key_down('Right'):
             self.direction = "right"
-            self.x += 20
+            self.x += 15
+            
 Player = Player(450, 450)
 Prova = Prova(450, 30)
 
